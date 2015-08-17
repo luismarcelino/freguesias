da2d78cbe457fbdcc8b9fccc2d5250dfb1b36952
Luis Marcelinio <luismarcelino@gmail.com>
2015-08-17 19:18:44 +0100

    Names space change

---
+++ src/Luismarcelino/Freguesias/FreguesiasFacade.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Luismarcelino\Freguesias;
+
+use Illuminate\Support\Facades\Facade;
+
+/**
+ * FreguesiasFacade
+ *
+ */
+class FreguesiasFacade extends Facade {
+
+    /**
+     * Get the registered name of the component.
+     *
+     * @return string
+     */
+    protected static function getFacadeAccessor() { return 'freguesias'; }
+
+}
